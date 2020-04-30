62c90105cbd98a9d5e1f77d89e9ae459088b0d35
akshaychinnan <63932860+akshaychinnan@users.noreply.github.com>
2020-05-01 02:00:47 +0530

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -44,7 +44,7 @@ async def youtube_dl_call_back(bot, update):
             cache_time=0
         )
         return False, None
-    user_working_dir = os.path.join(DOWNLOAD_LOCATION, str(current_user_id), str(time.time()))
+    user_working_dir = os.path.join(DOWNLOAD_LOCATION, str(current_user_id))
     # create download directory, if not exist
     if not os.path.isdir(user_working_dir):
         await bot.delete_messages(
@@ -105,8 +105,7 @@ async def youtube_dl_call_back(bot, update):
     #
     tmp_directory_for_each_user = os.path.join(
         DOWNLOAD_LOCATION,
-        str(update.from_user.id),
-        str(time.time())
+        str(update.from_user.id)
     )
     if not os.path.isdir(tmp_directory_for_each_user):
         os.makedirs(tmp_directory_for_each_user)
