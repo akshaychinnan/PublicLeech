9c038f7ae5d42428a345bd02dd6465ff26be5887
akshaychinnan <63932860+akshaychinnan@users.noreply.github.com>
2020-05-01 20:22:03 +0530

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -84,16 +84,16 @@ async def youtube_dl_call_back(bot, update):
     # https://superuser.com/a/994060
     LOGGER.info(custom_file_name)
     #
-    if "noyes.in" in youtube_dl_url or "tor.checker.in" in youtube_dl_url or "workers.dev" in youtube_dl_url:
-        await update.message.edit_caption(
-            caption="😡😡 <i>please do not abuse this <u>FREE</u> service</i> 🌚"
-        )
-        return
-    if "drive.google.com" in youtube_dl_url and youtube_dl_format != "source":
-        await update.message.edit_caption(
-            caption="<i>please do not abuse this <u>FREE</u> service</i>"
-        )
-        return
+    #if "noyes.in" in youtube_dl_url or "tor.checker.in" in youtube_dl_url or "workers.dev" in youtube_dl_url:
+    #    await update.message.edit_caption(
+    #        caption="😡😡 <i>please do not abuse this <u>FREE</u> service</i> 🌚"
+    #    )
+    #    return
+    #if "drive.google.com" in youtube_dl_url and youtube_dl_format != "source":
+    #    await update.message.edit_caption(
+    #        caption="<i>please do not abuse this <u>FREE</u> service</i>"
+    #    )
+    #    return
     #
     await update.message.edit_caption(
         caption="trying to download"
