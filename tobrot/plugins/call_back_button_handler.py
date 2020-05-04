3f83f246ffa168e25367fd5bfb0a282e8eb48027
akshaychinnan <63932860+akshaychinnan@users.noreply.github.com>
2020-05-04 22:10:09 +0530

    Update call_back_button_handler.py

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -13,10 +13,10 @@ from tobrot.helper_funcs.youtube_dl_button import youtube_dl_call_back
 
 
 async def button(bot, update):
-    LOGGER.info("===========================================CALL BACK update")
-    LOGGER.info(update)
-    LOGGER.info(update.data)
-    LOGGER.info("===========================================CALL BACK update")
+    #LOGGER.info("===========================================CALL BACK update")
+    #LOGGER.info(update)
+    #LOGGER.info(update.data)
+    #LOGGER.info("===========================================CALL BACK update")
     cb_data = update.data
     if "|" in cb_data:
         await youtube_dl_call_back(bot, update)
