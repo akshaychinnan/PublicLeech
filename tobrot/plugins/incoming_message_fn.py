fa338a38f1ffe34c19cb98277f266faa046032a4
akshaychinnan <63932860+akshaychinnan@users.noreply.github.com>
2020-05-12 13:10:07 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -118,7 +118,7 @@ async def incoming_purge_message_f(client, message):
         # Show All Downloads
         downloads = aria_i_p.get_downloads()
         for download in downloads:
-            LOGGER.info(download.remove(force=True))
+            LOGGER.info(download.remove(force=True, files=True, clean=True))
     await i_m_sefg2.delete()
 
 async def incoming_message_f(client, message):
